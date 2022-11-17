c7940a66074aa9c8fd605dea28c567d82437b585
KimJaeGeun <jgk9612@gmail.com>
2022-11-17 11:55:34 +0900

    first commit

---
+++ src/main/java/com/mustache/ptbbs/PtbbsApplication.java
@@ -0,0 +1,13 @@
+package com.mustache.ptbbs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PtbbsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PtbbsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mustache/ptbbs/PtbbsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustache.ptbbs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PtbbsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
